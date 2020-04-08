7c7f2b3aa46d25d00ca6992f0aa2bf6a184449fe
seitaro.ogiri <seitaro.0811@icloud.com>
2020-04-08 13:47:53 +0900

    Add Comment model

---
+++ src/models/Comment.java
@@ -0,0 +1,78 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "comments")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllComments",
+            query = "SELECT c FROM Comment AS c ORDER BY c.id DESC"
+            ),
+    @NamedQuery(
+            name = "getCommentsCount",
+            query = "SELECT COUNT(c) FROM Comment AS c"
+            )
+})
+
+@Entity
+public class Comment {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Report getReport() {
+        return report;
+    }
+
+    public void setReport(Report report) {
+        this.report = report;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreate_at() {
+        return create_at;
+    }
+
+    public void setCreate_at(Timestamp create_at) {
+        this.create_at = create_at;
+    }
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "report_id", nullable = false)
+    private Report report;
+
+    @Lob
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "create_at", nullable = false)
+    private Timestamp create_at;
+}
